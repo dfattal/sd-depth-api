d353b0921a7d2e34c458ae194a85d019ace455b4
David <david.fattal@leiainc.com>
2024-08-04 17:53:18 -0700

    changed prompt to "realistic photography"

--- process_image.py
+++ process_image.py
@@ -26,7 +26,7 @@ def process_image(depth_image_bytes):
     depth_image = Image.open(io.BytesIO(depth_image_bytes)).convert("RGB")
     controlnet_conditioning_scale = 0.5
 
-    prompt = ""
+    prompt = "realistic photography"
     images = pipe(
         prompt, image=depth_image, num_inference_steps=30, controlnet_conditioning_scale=controlnet_conditioning_scale,
     ).images
